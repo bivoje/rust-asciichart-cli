e2967346c19db045653e5557f15aaa0887e3098a
bivoje <bivoje49@gmail.com>
2022-07-23 21:10:47 +0900

    Organize config handler routines

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 
-use rasciigraph::{plot};
-use clap::{Parser};
+use clap::Parser;
 use itertools::Itertools;
 
 #[derive(Parser, Debug)]
 #[clap(name = "asciichart-cui")]
 #[clap(author, version, about, long_about = None)] // read from Cargo.toml
 struct Args {
-    #[clap(short, long, value_parser, default_value_t=0)]
-    width: u32,
 
-    #[clap(short, long, value_parser, default_value_t=0)]
-    height: u32,
+    #[clap(long, value_parser)]
+    ymax: Option<f64>,
+
+    #[clap(long, value_parser)]
+    ymin: Option<f64>,
+
+    #[clap(short, long, value_parser)]
+    width: Option<usize>,
+
+    #[clap(short, long, value_parser)]
+    height: Option<usize>,
+
+    #[clap(long, value_parser, validator=validate_tileset, arg_enum)]
+    tileset: Option<String>,
 
     #[clap(short, long, value_parser, arg_enum, default_value_t=Mode::Fast)]
     // FIXME arg_enum attribute makes clap don't require Display implementation on Mode
@@ -26,8 +35,12 @@ enum Mode {
     Fast, Slow
 }
 
+fn validate_tileset(s :&str) -> Result<(), &'static str> {
+    if s.len() == 10 {Ok(())} else {Err("should be of length 10")}
+}
+
 fn main() {
-    let _args = Args::parse();
+    let args = Args::parse();
 
     let width = 80;
     let mut v = vec![0f64; width];
@@ -36,24 +49,31 @@ fn main() {
         //v[i] = 3000. +  0.000001 * (i as f64 * pi * 4.0 / 120.0).sin();
         v[i] = 100. + 0.01 * (i as f64 * pi * 4.0 / width as f64).sin();
     }
+    let vss = vec![(v,1)];
 
-    //let _ret = asciichart_plot(vec![(vec![1.],3)], Config {symbols: DEFAULT_SYMBOLS, width: None, height: None});
-    let _ret = asciichart_plot(&vec![(v.clone(),1)], Config {
-        symbols: DEFAULT_SYMBOLS, width: None, height: Some(9)
-    });
+    let cfg = gen_config(&vss, args).unwrap(); // FIXME
+    let _ret = plot(&vss, cfg);
 
-    println!( "{}", plot(v,
+    println!( "{}", rasciigraph::plot(vss[0].0.clone(),
         rasciigraph::Config::default().with_height(8)
     ));
 }
 
 const DEFAULT_SYMBOLS: [char; 10] = ['┼', '┤', '╶', '╴', '─', '╰', '╭', '╮', '╯', '│'];
 
+#[derive(Debug, Default)]
 struct Config {
     symbols: [char; 10], // defaults to DEFAULT_SYMBOLS
-    width: Option<usize>, // None for variate
-    height: Option<usize>, // None for integer scheme; default = 5
+    width: usize, // None for variate
+    height: usize, // None for integer scheme; default = 5
     // what if w=0 or h=0?
+
+    v_bot: f64,
+    v_top: f64,
+    v_step: f64,
+
+    label_bodywidth: usize,
+    label_precision: usize,
 }
 
 
@@ -65,48 +85,67 @@ fn max_f64<T> (iter: T) -> Option<f64> where T: Iterator<Item=f64> {
     iter.filter(|v|!v.is_nan() && !v.is_infinite()).reduce(|a,v| if v>a {v} else {a})
 }
 
-//if vs.is_empty() {iter::once(f32::)}
-fn asciichart_plot(vss: &Vec<(Vec<f64>,u32)>, cfg: Config) -> Result<String, &'static str> {
+// handles generates configs, calculates defaults that are data-related
+fn gen_config(vss: &Vec<(Vec<f64>,u32)>, args: Args) -> Option<Config> {
 
-    let (v_min, v_max) = {
+    let (v_bot, v_top) = {
         let nan = std::f64::NAN;
         let min = min_f64(vss.iter().map(|(vs,_)| min_f64(vs.iter().cloned()).unwrap_or(nan)));
         let max = max_f64(vss.iter().map(|(vs,_)| max_f64(vs.iter().cloned()).unwrap_or(nan)));
 
         if let (Some(min), Some(max)) = (min, max) {
-            (min, max)
+            (args.ymin.unwrap_or(min), args.ymax.unwrap_or(max))
         } else {
-            return Err("empty graph");
+            return None;
         }
     };
-    println!("min:{} max:{}", v_min, v_max);
+    println!("min:{} max:{}", v_bot, v_top);
 
-    let v_interval = v_max - v_min; // >= 0
-    let height = cfg.height.unwrap_or(1 + v_interval.floor() as usize); // >= 1
+    let width = args.width.unwrap_or(vss.iter().map(|vs| vs.0.len()).min().unwrap_or(0));
+
+    let v_interval = v_top - v_bot; // >= 0
+    let height = args.height.unwrap_or(1 + v_interval.floor() as usize); // >= 1
+    println!("width:{} height:{}", width, height);
+
     // use integer mode when height is not specified
     let v_step = if height == 1 {0.} else { v_interval / (height-1) as f64 };
     println!("intv:{} h:{} step:{}", v_interval, height, v_step);
 
+    let label_bodywidth = 1 + v_top.abs().max(v_bot.abs()).log10().floor() as usize;
     let label_precision = {
         let prec = 1 - v_step.log10().floor() as i32;
-        (if cfg.height.is_none() {0} else {1}).max(prec) as usize
+        (if args.height.is_none() {0} else {1}).max(prec) as usize
         // force prec >= 1 unless height=None (integer mode)
     };
-    let label_bodywidth = 1 + v_max.abs().max(v_min.abs()).log10().floor() as usize;
     // FIXME what if 0???
     println!("<{}>.<{}>", label_bodywidth, label_precision);
 
-    let width = cfg.width.unwrap_or(vss.iter().map(|vs| vs.0.len()).min().unwrap_or(0));
+    let mut cfg = Config {
+        symbols: DEFAULT_SYMBOLS,
+        v_bot: v_bot, v_top: v_top, v_step: v_step, width: width, height: height,
+        label_bodywidth: label_bodywidth, label_precision: label_precision,
+    };
+
+    if let Some(tileset) = args.tileset {
+        for (i,c) in tileset.chars().enumerate() { cfg.symbols[i] = c; }
+    }
+
+    Some(cfg)
+}
+
+fn plot(vss: &Vec<(Vec<f64>,u32)>, cfg: Config) -> String {
+    // TODO check if v_step is positive
 
     let label_margin = {
-        let abs_width = label_bodywidth + 1 + label_precision; // add 1 for midpoint
+        let abs_width = cfg.label_bodywidth + 1 + cfg.label_precision; // add 1 for midpoint
         // left space 1, neg sign 1/0, the number with ljust, right space 1
-        1 + if v_min < 0. {1} else {0} + abs_width + 1
+        1 + if cfg.v_bot < 0. {1} else {0} + abs_width + 1
     };
-    let mut buffer = vec![vec![(' ', 9); label_margin + width]; 1+height];
-    println!("width:{} lm:{}", width, label_margin);
 
-    for y in 0..height { // never executed when height == 1, in which case min==max
+    let mut buffer = vec![vec![(' ', 9); label_margin + cfg.width]; 1+cfg.height];
+    println!("width:{} lm:{}", cfg.width, label_margin);
+
+    for y in 0..cfg.height { // never executed when height == 1, in which case min==max
         // String uses Vec[u8] in specified (default UTF-8) encoding internally,
         // while hiding the internals to outer codes.
         // write_* functions produces u8 stream,
@@ -115,11 +154,11 @@ fn asciichart_plot(vss: &Vec<(Vec<f64>,u32)>, cfg: Config) -> Result<String, &'s
         // format! creates temporary String,
         // this is wasteful yet I couldn't find way to get char stream from format!.
         // https://stackoverflow.com/a/24542502
-        let number = v_min + (y as f64) * v_step;
+        let number = cfg.v_bot + (y as f64) * cfg.v_step;
         let mut label = format!(
             "{number:LW$.PREC$} ",
-            LW = label_bodywidth,
-            PREC = label_precision,
+            LW = cfg.label_bodywidth,
+            PREC = cfg.label_precision,
             number = number,
         );
         let offset = label_margin - label.len(); // label is all ascii, 1 byte per char
@@ -131,14 +170,16 @@ fn asciichart_plot(vss: &Vec<(Vec<f64>,u32)>, cfg: Config) -> Result<String, &'s
 
     // FIXME what about single row?? what about inf values?
     //let clamp = |v| v.max(v_max + 1.).min(v_min - 1.)
-    let scaled = |v :f64| (!v.is_nan()).then_some(((v-v_min)/v_step).round() as usize);
+    let scaled = |v :f64| (!v.is_nan()).then_some(((v-cfg.v_bot)/cfg.v_step).round() as usize);
     // what about INF?
 
+    // margin + axis char 1
     let offset = label_margin + 1;
+
     for (vs,color) in vss {
         let color = *color;
         // FIXME use .take(n)
-        let vvs = vs[..width.min(vs.len())].into_iter().cloned().tuple_windows();
+        let vvs = vs[..cfg.width.min(vs.len())].into_iter().cloned().tuple_windows();
         for (x,(v0,v1)) in vvs.enumerate() {
 
             match (scaled(v0), scaled(v1)) {
@@ -175,5 +216,5 @@ fn asciichart_plot(vss: &Vec<(Vec<f64>,u32)>, cfg: Config) -> Result<String, &'s
     }
 
     print!("{}", ret);
-    Ok(ret)
+    ret
 }
